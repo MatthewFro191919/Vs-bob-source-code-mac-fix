63ddb8eb610199dcc0a5a3f9199cf516472ba767
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-08-26 07:47:28 -0400

    Update KadeEngineData.hx

--- sourceJon/KadeEngineData.hx
+++ sourceJon/KadeEngineData.hx
@@ -54,6 +54,12 @@ class KadeEngineData
 		if (FlxG.save.data.frames == null)
 			FlxG.save.data.frames = 10;
 
+		if (FlxG.save.data.botplay == null)
+			FlxG.save.data.botplay = false;
+
+		if (FlxG.save.data.cpuStrums == null)
+			FlxG.save.data.cpuStrums = false;
+
 		Conductor.recalculateTimings();
 
 		(cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
